b6810b1754fba734a3a507b3f31eae9f6c0a685b
Matthias Breuer <mail@matthiasbreuer.com>
2012-10-03 13:10:57 +0200

    Pleases JSHint

--- src/actions.js
+++ src/actions.js
@@ -12,149 +12,172 @@
  * @type {object}
  */
 var actions = {
-	hash: {
-		_uid: 0
-	}
+    _hash:{},
+    _uidIndex:0
 };
 
 /**
  * Helps gettings the right namespace and name
  * @param {string|object} ident Identifier of the action, can have a namespace e.g. 'namespace:action' as string or an object with 'ns' and 'name' keys.
  */
-actions._split = function(ident) {
-	var split = {
-		ns: 'global',
-		name: ident
-	}
-	if( typeof ident == 'object') {
-		split.ns = ident.ns;
-		split.name = ident.name;
-	} else if( typeof ident == 'string') {
-		var index = ident.indexOf(':');
-		if(index != -1) {
-			split.ns = ident.slice(0, index);
-			split.name = ident.slice(index + 1, ident.length);
-		}
-	}
-	return split;
-}
+actions._split = function (ident) {
+    'use strict';
+    var split = {
+        ns:'global',
+        name:ident
+    };
+    if (typeof ident === 'object') {
+        split.ns = ident.ns;
+        split.name = ident.name;
+    } else if (typeof ident === 'string') {
+        var index = ident.indexOf(':');
+        if (index !== -1) {
+            split.ns = ident.slice(0, index);
+            split.name = ident.slice(index + 1, ident.length);
+        }
+    }
+    return split;
+};
 
 /**
-* Invokes action
-* @param {string|object} ident Identifier of the action, can have a namespace e.g. 'namespace:action' as string or an object with 'ns' and 'name' keys.
-*/
-actions.doAction = function(ident) {
-	ident = actions._split(ident);
-	log(ident.ns + ':' + ident.name, arguments);
-	if(actions.hash[ident.ns] && actions.hash[ident.ns][ident.name]) {
-		var callbacks = actions.hash[ident.ns][ident.name];
-		var args = Array.prototype.slice.call(arguments).splice(1);
-		for(var i = 0; i < callbacks.length; i++) {
-			try {
-				callbacks[i].callback.apply(callbacks[i].thisArg, args);
-			} catch (e) {
-				actions.removeAction(ident, callbacks[i].id)
-			}
-		}
-	}
-}
+ * Returns a unique ID
+ */
+actions._uid = function () {
+    'use strict';
+    actions._uidIndex += 1;
+    return actions._uidIndex;
+};
 
 /**
-* Registers an action.
-* @param {string|object} ident Identifier of the action, can have a namespace e.g. 'namespace:action' as string or an object with 'ns' and 'name' keys.
-* @param {function} callback Callback fucntion
-* @param {object} thisArg Optional this scope
-* @param {number} priority Priority of the callback (default: 10)
-* @return {number} ID of the callback
-*/
-actions.addAction = function(ident, callback, thisArg, priority) {
-	function indexOf(haystack, callback) {
-		for(var i = 0; i < haystack.length; i++) {
-			//if(haystack[i].callback == callback && haystack[i].thisArg == thisArg) {
-			if(haystack[i].callback == callback) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	ident = actions._split(ident);
-
-	var args = {
-		callback : callback,
-		thisArg : thisArg ? thisArg : window,
-		priority : priority ? priority : 10,
-		id: actions.hash._uid++
-	};
-	if(actions.hash[ident.ns] && actions.hash[ident.ns][ident.name]) {
-		if(indexOf(actions.hash[ident.ns][ident.name], callback) == -1) {
-			actions.hash[ident.ns][ident.name].push(args);
-			actions.hash[ident.ns][ident.name].sort(function(a, b) {
-				return (a.priority > b.priority) - (a.priority < b.priority);
-			});
-		} else {
-			return actions.hash[ident.ns][ident.name][indexOf(actions.hash[ident.ns][ident.name], callback)].id;
-		}
-	} else {
-		if(!actions.hash[ident.ns]) {
-			actions.hash[ident.ns] = {};
-		}
-		actions.hash[ident.ns][ident.name] = new Array(args);
-	}
-	return args.id;
-}
+ * Invokes action
+ * @param {string|object} ident Identifier of the action, can have a namespace e.g. 'namespace:action' as string or an object with 'ns' and 'name' keys.
+ */
+actions.doAction = function (ident) {
+    'use strict';
+    ident = actions._split(ident);
+    console.log(ident.ns + ':' + ident.name, arguments);
+    if (actions._hash[ident.ns] && actions._hash[ident.ns][ident.name]) {
+        var items = actions._hash[ident.ns][ident.name];
+        var args = Array.prototype.slice.call(arguments).splice(1);
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (!item) {
+                continue;
+            }
+            try {
+                item.callback.apply(item.thisArg, args);
+                if (item.once) {
+                    actions.removeAction(ident, item.id);
+                }
+            } catch (e) {
+                console.log(e.message);
+                actions.removeAction(ident, item.id);
+            }
+        }
+    }
+};
 
 /**
-* Removes an action callback.
-* @param {string|object} ident Identifier of the actions
-* @param {function|number} callback Callback function or ID
-* @return {boolean} True (success) or false (not found)
-*/
-actions.removeAction= function(ident, callback) {
-	function indexOf(haystack, callback) {
-		var func = typeof callback === 'function';
-		for(var i = 0; i < haystack.length; i++) {
-			if(func) {
-				if(haystack[i].callback == callback) {
-					return i;
-				}
-			} else {
-				if(haystack[i].id == callback) {
-					return i;
-				}
-			}
-		}
-		return -1;
-	}
+ * Registers an action.
+ * @param {string|object} ident Identifier of the action, can have a namespace e.g. 'namespace:action' as string or an object with 'ns' and 'name' keys.
+ * @param {function} callback Callback fucntion
+ * @param {object} thisArg Optional this scope
+ * @param {number} priority Priority of the callback (default: 10)
+ * @param {boolean} once Remove action after invoking it once
+ * @return {number} ID of the callback
+ */
+actions.addAction = function (ident, callback, thisArg, priority, once) {
+    'use strict';
+    function indexOf(haystack, callback) {
+        for (var i = 0; i < haystack.length; i++) {
+            //if(haystack[i].callback == callback && haystack[i].thisArg == thisArg) {
+            if (haystack[i].callback === callback) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	ident = actions._split(ident);
-	
-	if(actions.hash[ident.ns] && actions.hash[ident.ns][ident.name]) {
-		if(callback == undefined) {
-			delete actions.hash[ident.ns][ident.name];
-			return true;
-		}
-		var index = indexOf(actions.hash[ident.ns][ident.name], callback);
-		if(index != -1) {
-			actions.hash[ident.ns][ident.name].splice(index, 1);
-			return true;
-		} else {
-			return false;
-		}
-	} else {
-		return false;
-	}
-}
+    ident = actions._split(ident);
+
+    var args = {
+        callback:callback,
+        thisArg:thisArg ? thisArg : window,
+        priority:priority ? priority : 10,
+        id:actions._uid(),
+        once:once !== undefined
+    };
+    if (actions._hash[ident.ns] !== undefined && actions._hash[ident.ns][ident.name] !== undefined) {
+        if (indexOf(actions._hash[ident.ns][ident.name], callback) === -1) {
+            actions._hash[ident.ns][ident.name].push(args);
+            actions._hash[ident.ns][ident.name].sort(function (a, b) {
+                return (a.priority > b.priority) - (a.priority < b.priority);
+            });
+        } else {
+            return actions._hash[ident.ns][ident.name][indexOf(actions._hash[ident.ns][ident.name], callback)].id;
+        }
+    } else {
+        if (actions._hash[ident.ns] === undefined) {
+            actions._hash[ident.ns] = {};
+        }
+        actions._hash[ident.ns][ident.name] = [args];
+    }
+    return args.id;
+};
+
+/**
+ * Removes an action callback.
+ * @param {string|object} ident Identifier of the actions
+ * @param {function|number} callback Callback function or ID
+ * @return {boolean} True (success) or false (not found)
+ */
+actions.removeAction = function (ident, callback) {
+    'use strict';
+    function indexOf(haystack, callback) {
+        var func = typeof callback === 'function';
+        for (var i = 0; i < haystack.length; i++) {
+            if (func) {
+                if (haystack[i].callback === callback) {
+                    return i;
+                }
+            } else {
+                if (haystack[i].id === callback) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    ident = actions._split(ident);
+
+    if (actions._hash[ident.ns] !== undefined && actions._hash[ident.ns][ident.name] !== undefined) {
+        if (callback === undefined) {
+            delete actions._hash[ident.ns][ident.name];
+            return true;
+        }
+        var index = indexOf(actions._hash[ident.ns][ident.name], callback);
+        if (index !== -1) {
+            actions._hash[ident.ns][ident.name].splice(index, 1);
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+};
 
 /**
  * Removes a whole namespace
  * @param {string} ns The namespace identifier
  * @return {boolean} true (success) or false (not found)
  */
-actions.removeNamespace = function(ns) {
-	if(actions.hash[ns]) {
-		delete actions.hash[ns];
-		return true;
-	}
-	return false;
-}
+actions.removeNamespace = function (ns) {
+    'use strict';
+    if (actions._hash[ns]) {
+        delete actions._hash[ns];
+        return true;
+    }
+    return false;
+};
